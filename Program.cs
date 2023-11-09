4cf02efacc1a8b47c8b8477dab4ea15e89ba1aa5
fishman123456 <den9152@yandex.ru>
2023-11-10 00:08:40 +0700

    rfr

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace shambalaRegistryExample_09_11_2023
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            //Console.OutputEncoding = Encoding.UTF8;
             //        Console.WriteLine(Registry.CurrentUser.Name);
             //        Console.ReadKey();
             //        string[] currentUserSubKeyNames = Registry.CurrentUser.GetSubKeyNames();
@@ -25,22 +25,36 @@ namespace shambalaRegistryExample_09_11_2023
             //        foreach (string key in classesRootSubKeyNames)
             //        { Console.WriteLine($"\t{key}"); }
             //Console.ReadKey();
-            PrintSubKey(Registry.CurrentUser);
-            PrintSubKey(Registry.ClassesRoot);
-            PrintSubKey(Registry.Users);
-            PrintSubKey(Registry.CurrentConfig);
-            PrintSubKey(Registry.PerformanceData);
-            PrintSubKey(Registry.DynData);
-
+            //PrintSubKey(Registry.CurrentUser);
+            //PrintSubKey(Registry.ClassesRoot);
+            //PrintSubKey(Registry.Users);
+            //PrintSubKey(Registry.CurrentConfig);
+            //PrintSubKey(Registry.PerformanceData);
+            //PrintSubKey(Registry.DynData);
+            PrintSubKey(Registry.CurrentUser.OpenSubKey("Printers\\ConvertUserDevModesCount"));
         }
-         static void PrintSubKey(RegistryKey key)
+        static void PrintSubKey(RegistryKey key)
         {
-            { Console.WriteLine($"\t{key}"); }
-            string[] classesRootSubKeyNames = key.GetSubKeyNames();
-            foreach (string keys in classesRootSubKeyNames)
-            { Console.WriteLine($"\t{keys}"); }
-            Console.ReadKey();
+            try
+            { 
+                    Console.WriteLine($"key: {key.Name}");
+                    Console.WriteLine($"value: {key}");
+                    string[] valueNames = key.GetValueNames();
+                    for (int i = 0; i < key.ValueCount; i++)
+                    {
+                        Console.WriteLine($"{valueNames[i]} = {key.GetValue(valueNames[i])} ");
+                    }
+
+                    string[] classesRootSubKeyNames = key.GetSubKeyNames();
+                foreach (string keys in classesRootSubKeyNames)
+                { Console.WriteLine($"\t{keys}"); }
+                Console.ReadKey();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
-    
 }
