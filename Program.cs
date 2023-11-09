ffbdbd25109429d1d6da460724b2a6a085212d6d
fishman123456 <den9152@yandex.ru>
2023-11-10 01:13:31 +0700

    взять для домашки

--- Program.cs
+++ Program.cs
@@ -8,71 +8,71 @@ using System.Threading.Tasks;
 
 namespace shambalaRegistryExample_09_11_2023
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            #region
-            //Console.OutputEncoding = Encoding.UTF8;
-            //        Console.WriteLine(Registry.CurrentUser.Name);
-            //        Console.ReadKey();
-            //        string[] currentUserSubKeyNames = Registry.CurrentUser.GetSubKeyNames();
-            //        foreach (string key in currentUserSubKeyNames)
-            //        { Console.WriteLine($"\t{key}"); }
-            //    Console.ReadKey();
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        #region
+    //        //Console.OutputEncoding = Encoding.UTF8;
+    //        //        Console.WriteLine(Registry.CurrentUser.Name);
+    //        //        Console.ReadKey();
+    //        //        string[] currentUserSubKeyNames = Registry.CurrentUser.GetSubKeyNames();
+    //        //        foreach (string key in currentUserSubKeyNames)
+    //        //        { Console.WriteLine($"\t{key}"); }
+    //        //    Console.ReadKey();
 
 
-            //    string[] classesRootSubKeyNames = Registry.ClassesRoot.GetSubKeyNames();
-            //        foreach (string key in classesRootSubKeyNames)
-            //        { Console.WriteLine($"\t{key}"); }
-            //Console.ReadKey();
-            //PrintSubKey(Registry.CurrentUser);
-            //PrintSubKey(Registry.ClassesRoot);
-            //PrintSubKey(Registry.Users);
-            //PrintSubKey(Registry.CurrentConfig);
-            //PrintSubKey(Registry.PerformanceData);
-            //PrintSubKey(Registry.DynData);
-            #endregion
-            //PrintSubKey(Registry.CurrentUser.OpenSubKey("Printers\\ConvertUserDevModesCount"));
+    //        //    string[] classesRootSubKeyNames = Registry.ClassesRoot.GetSubKeyNames();
+    //        //        foreach (string key in classesRootSubKeyNames)
+    //        //        { Console.WriteLine($"\t{key}"); }
+    //        //Console.ReadKey();
+    //        //PrintSubKey(Registry.CurrentUser);
+    //        //PrintSubKey(Registry.ClassesRoot);
+    //        //PrintSubKey(Registry.Users);
+    //        //PrintSubKey(Registry.CurrentConfig);
+    //        //PrintSubKey(Registry.PerformanceData);
+    //        //PrintSubKey(Registry.DynData);
+    //        #endregion
+    //        //PrintSubKey(Registry.CurrentUser.OpenSubKey("Printers\\ConvertUserDevModesCount"));
             
-        }
-        static void PrintSubKey(RegistryKey key)
-        {
-            try
-            { 
-                    Console.WriteLine($"key: {key.Name}");
-                    Console.WriteLine($"value: {key}");
-                    string[] valueNames = key.GetValueNames();
-                    for (int i = 0; i < key.ValueCount; i++)
-                    {
-                        Console.WriteLine($"{valueNames[i]} = {key.GetValue(valueNames[i])} ");
-                    }
+    //    }
+    //    static void PrintSubKey(RegistryKey key)
+    //    {
+    //        try
+    //        { 
+    //                Console.WriteLine($"key: {key.Name}");
+    //                Console.WriteLine($"value: {key}");
+    //                string[] valueNames = key.GetValueNames();
+    //                for (int i = 0; i < key.ValueCount; i++)
+    //                {
+    //                    Console.WriteLine($"{valueNames[i]} = {key.GetValue(valueNames[i])} ");
+    //                }
 
-                    string[] classesRootSubKeyNames = key.GetSubKeyNames();
-                foreach (string keys in classesRootSubKeyNames)
-                { Console.WriteLine($"\t{keys}"); }
-                Console.ReadKey();
+    //                string[] classesRootSubKeyNames = key.GetSubKeyNames();
+    //            foreach (string keys in classesRootSubKeyNames)
+    //            { Console.WriteLine($"\t{keys}"); }
+    //            Console.ReadKey();
                 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void CreateKeyWithValue()
-        {
-            // создать под ключ  в каком либо ключе
-            RegistryKey key = Registry.CurrentUser.CreateSubKey("new_key");
-            // закрыть ключ
-            key.Close();
-        }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //    }
+    //    static void CreateKeyWithValue()
+    //    {
+    //        // создать под ключ  в каком либо ключе
+    //        RegistryKey key = Registry.CurrentUser.CreateSubKey("new_key");
+    //        // закрыть ключ
+    //        key.Close();
+    //    }
 
-        static void GetandChengeKeyValue()
-        {
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("new_key", true))
-            {
+    //    static void GetandChengeKeyValue()
+    //    {
+    //        using (RegistryKey key = Registry.CurrentUser.OpenSubKey("new_key", true))
+    //        {
                 
-            }
-        }
-    }
+    //        }
+    //    }
+    //}
 }
